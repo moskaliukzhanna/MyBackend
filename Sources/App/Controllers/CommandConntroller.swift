aa428621e081e4f086e2ab0c81d83186f62576f3
Zhanna Moskaliuk <octoberrock11@gmail.com>
2023-02-21 09:26:55 +0200

    Clean up

--- Sources/App/Controllers/CommandConntroller.swift
+++ Sources/App/Controllers/CommandConntroller.swift
@@ -11,20 +11,12 @@ import Vapor
 final class CommandConntroller {
     var commands: [OutcomingMessage] = []
     var commandsCount = 0
-//    var socketsUUIDDict: [ConnectionSource: [String: URL]] = [:]
     
     private func appendCommands() {
         commands.append(OutcomingMessage(method: .outcomingMessage, id: 1234, path: .launch, data: Details(timeout: 0)))
-        commands.append(OutcomingMessage(method: .outcomingMessage, id: 1234, path: .pressHomeButton))
-        commands.append(OutcomingMessage(method: .outcomingMessage, id: 1234, path: .homeScreenPress))
-        commands.append(OutcomingMessage(method: .outcomingMessage, id: 1234, path: .homeScreenEdit))
-        
-//        Task {
-//            for command in commands {
-//                try await Task.sleep(nanoseconds: 10000000000)
-////                executeCommand(message: command)
-//            }
-//        }
+        commands.append(OutcomingMessage(method: .outcomingMessage, id: 12345, path: .pressHomeButton))
+        commands.append(OutcomingMessage(method: .outcomingMessage, id: 12346, path: .homeScreenPress))
+        commands.append(OutcomingMessage(method: .outcomingMessage, id: 12347, path: .homeScreenEdit))
     }
     
      func handleGETRequest(app: Application) {
@@ -32,10 +24,6 @@ final class CommandConntroller {
         app.get("command") { [weak self] req -> OutcomingMessage in
             let defaultCommand = OutcomingMessage(method: .outcomingMessage, id: 1234, path: .launch, data: Details(timeout: 0))
             guard let self = self else { return defaultCommand }
-//            guard let name = req.parameters.get("name") else {
-//                throw Abort(.internalServerError)
-//            }
-            
             guard let command = self.commands.first else { return defaultCommand }
             self.commands.removeFirst()
             return command
